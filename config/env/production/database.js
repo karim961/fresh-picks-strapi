5e0797b683afe217fae8a6d6b074219d9022addc
Karim Abbas <32097461+karim961@users.noreply.github.com>
2022-02-10 18:35:38 +0200

    mongodb atlas configuration

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,24 +1,13 @@
-
-
-
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'mongoose',
       settings: {
-        client: 'mongo',
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'strapi'),
-        username: env('DATABASE_USERNAME', ''),
-        password: env('DATABASE_PASSWORD', ''),
-        ssl: {
-          rejectUnauthorized: false
-        }
+        uri: env('DATABASE_URI',),
       },
       options: {
-        ssl: false,
+        ssl: env.bool('DATABASE_SSL', true),
       },
     },
   },
