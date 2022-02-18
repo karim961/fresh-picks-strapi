aec12f15aa76da2ac9719ada2e7b22b11453832b
Karim Abbas <32097461+karim961@users.noreply.github.com>
2022-02-19 01:19:24 +0200

    feat: add footer

--- api/categories/models/categories.js
+++ api/categories/models/categories.js
@@ -4,5 +4,22 @@
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
  * to customize this model
  */
+const slugify = require('slugify');
 
-module.exports = {};
+module.exports = {
+  /**
+   * Triggered before user creation.
+   */
+  lifecycles: {
+    async beforeCreate(data) {
+      if (data.title) {
+        data.slug = slugify(data.title, {lower: true});
+      }
+    },
+    async beforeUpdate(params, data) {
+      if (data.title) {
+        data.slug = slugify(data.title, {lower: true});
+      }
+    },
+  },
+};
